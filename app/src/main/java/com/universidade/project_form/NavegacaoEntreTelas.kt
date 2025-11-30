7ee3c3f1c4d030f866abea870f37be61dc75f67a
adilson-hernany <104524648+adilsonhernany04@users.noreply.github.com>
2025-11-30 19:01:22 +0100

    Refazer a navegação para incluir login + validação e usando snackbar

--- app/src/main/java/com/universidade/project_form/NavegacaoEntreTelas.kt
+++ app/src/main/java/com/universidade/project_form/NavegacaoEntreTelas.kt
@@ -1,17 +1,18 @@
 package com.universidade.project_form
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+// AUTHOR : Adilson Hernany
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import com.universidade.project_form.modelos.Produto
 import com.universidade.project_form.ui.telas.TelaCadastrarProduto
 import com.universidade.project_form.ui.telas.TelaLogin
+import kotlinx.coroutines.launch
 
 sealed class Telas(val rota: String) {
     object Login : Telas("login")
@@ -24,19 +25,60 @@ fun AppNavegacaoTelas(controladorNav: NavHostController) {
     // lista produtos
     var listaProduto by remember { mutableStateOf<List<Produto>>(emptyList()) }
 
-    NavHost(
-        navController = controladorNav,
-        startDestination = Telas.Cadastro.rota
-    ) {
-        // TELA DE CADASTRAR  PRODUTO
-        composable(Telas.Cadastro.rota) {
-            TelaCadastrarProduto(
-                aoRegistrarProduto = { produto ->
-                    // listaProduto = listaProduto + produto
-                    controladorNav.popBackStack()
-                },
-                aoVoltar = { controladorNav.popBackStack() }
-            )
+    val emailAdmin = "adilsonhernany@gmail.com"
+    val palPasseAdmin = "adilsonhernanyadmin"
+
+    // Snackbar
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) }
+    ) { paddingValues ->
+
+        NavHost(
+            navController = controladorNav,
+            startDestination = Telas.Login.rota,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            // TELA DE LOGIN
+            composable(Telas.Login.rota) {
+                TelaLogin(
+                    aoFazerLogin = { email, palPasse ->
+                        if (email == emailAdmin && palPasse == palPasseAdmin) {
+                            controladorNav.navigate(Telas.Cadastro.rota)
+                        } else {
+                            // MOSTRA SNACKBAR
+                            scope.launch {
+                                snackbarHostState.showSnackbar(
+                                    message = "Credenciais inválidas!",
+                                    duration = SnackbarDuration.Short
+                                )
+                            }
+                        }
+                    },
+                    aoNavegarParaRegistro = {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(
+                                message = "BY ADILSON HERNANY - COMPUTAÇÃO",
+
+                            )
+                        }
+                    }
+                )
+            }
+
+            // TELA DE CADASTRAR PRODUTO
+            composable(Telas.Cadastro.rota) {
+                TelaCadastrarProduto(
+                    aoRegistrarProduto = { produto ->
+                        listaProduto = listaProduto + produto
+                        controladorNav.popBackStack()
+                    },
+                    aoVoltar = { controladorNav.popBackStack() }
+                )
+            }
+
         }
     }
 }
